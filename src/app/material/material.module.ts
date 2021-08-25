7141338a73903b8d1368ca2a6eabaebf20159c78
Nikita Kharlap <harlap19@gmail.com>
2021-08-25 18:39:50 +0300

    try some material components

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -10,11 +10,24 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialAddressComponent } from './material-address/material-address.component';
 import { MaterialComponent } from "./material.component";
+import { TableComponent } from './table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  declarations:[
+  declarations: [
     MaterialAddressComponent,
     MaterialComponent,
+    TableComponent,
   ],
 
   imports: [
@@ -26,6 +39,17 @@ import { MaterialComponent } from "./material.component";
     MatRadioModule,
     MatCardModule,
     ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
   ]
 })
 export class MaterialModule {
